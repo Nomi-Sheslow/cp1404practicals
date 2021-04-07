1597f0284b6bece12ca0ad329eca98757c0bb354
Nomi-Sheslow <79426752+Nomi-Sheslow@users.noreply.github.com>
2021-04-07 15:02:00 +1000

    Add display_subject_details function, change get_data to return list_of_data

---
+++ prac_04/subject_reader.py
@@ -0,0 +1,40 @@
+"""
+CP1404/CP5632 Practical
+Data file -> lists program
+"""
+
+
+FILENAME = "subject_data.txt"
+
+
+def main():
+    data = get_data()
+    # print(data)
+    display_subject_details(data)
+
+
+def get_data():
+    """Read data from file formatted like: subject,lecturer,number of students."""
+    input_file = open(FILENAME)
+    list_of_data = []
+    for line in input_file:
+        print(line)  # See what a line looks like
+        print(repr(line))  # See what a line really looks like
+        line = line.strip()  # Remove the \n
+        list_of_data.append(line)
+        parts = line.split(',')  # Separate the data into its parts
+        print(parts)  # See what the parts look like (notice the integer is a string)
+        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        print(parts)  # See if that worked
+        print("----------")
+    input_file.close()
+    return list_of_data
+
+
+def display_subject_details(data):
+    for line in data:
+        line = line.split(",")
+        print(f"{line[0]} is taught by {line[1]:12} and has {line[2]:>3} students")
+
+
+main()
