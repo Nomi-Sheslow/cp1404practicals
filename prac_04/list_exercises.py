352453bcbc143f81aa2d810c05504afe376db364
Nomi-Sheslow <79426752+Nomi-Sheslow@users.noreply.github.com>
2021-04-07 20:40:48 +1000

    Add usernames list, user input, if-else statement for access being granted or not

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -15,3 +15,11 @@ print(f"The last number is {last_number}")
 print(f"The smallest number is {smallest_number}")
 print(f"The largest number is {largest_number}")
 print(f"The average of the numbers is {average}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
