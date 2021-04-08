d7b74e12bbf373a25674ee9c2a9bab619f23fce0
Nomi-Sheslow <79426752+Nomi-Sheslow@users.noreply.github.com>
2021-04-08 16:09:17 +1000

    Add COLOUR_TO_HEX dictionary, add doc string

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,9 @@
+"""
+CP1404/CP5632 Practical
+Hex colours in a dictionary
+get valid user input
+"""
+
+COLOUR_TO_HEX = {"black": "#000000", "brown": "#a52a2a", "VioletRed": "#d02090", "white": "#ffffff",
+"SteelBlue": "#4682b4", "SlateBlue": "#6a5acd", "SeaGreen1": "#54ff9f", "salmon": "#fa8072", "RoyalBlue4": "#27408b",
+"RosyBrown1": "#ffc1c1"}
