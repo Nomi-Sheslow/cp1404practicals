280fa978968373cf1c88e62ba0573e6c5556830e
Nomi-Sheslow <79426752+Nomi-Sheslow@users.noreply.github.com>
2021-04-08 16:21:05 +1000

    Add user input, add while loop, add if-else statement

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -7,3 +7,11 @@ get valid user input
 COLOUR_TO_HEX = {"black": "#000000", "brown": "#a52a2a", "VioletRed": "#d02090", "white": "#ffffff",
 "SteelBlue": "#4682b4", "SlateBlue": "#6a5acd", "SeaGreen1": "#54ff9f", "salmon": "#fa8072", "RoyalBlue4": "#27408b",
 "RosyBrown1": "#ffc1c1"}
+
+colour_name = input("Colour name: ")
+while colour_name != "":
+    if colour_name in COLOUR_TO_HEX:
+        print(f"{colour_name} hex code is {COLOUR_TO_HEX[colour_name]}")
+    else:
+        print("Not in dictionary")
+    colour_name = input("Colour name: ")
