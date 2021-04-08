56f6347e43cb87936b0b484acd736a4ecfc469e7
Nomi-Sheslow <79426752+Nomi-Sheslow@users.noreply.github.com>
2021-04-08 17:49:25 +1000

    Add while loop for email input, add while loop for confirmation input, add email & name to email_to_name dictionary, add for loop to print email_to_name dictionary

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,3 +1,15 @@
 email_to_name = {}
 emails = []
 email = input("Email: ")
+while email != "":
+    emails.append(email)
+    split_email = email.split("@")
+    name = split_email[0].replace(".", " ").title()
+    confirmation = input(f"Is your name {name}? (Y/n) ").lower()
+    while confirmation == "n":
+        name = input("Name: ").title()
+        confirmation = input(f"Is your name {name}? (Y/n) ").lower()
+    email_to_name[name] = email
+    email = input("Email: ")
+for name, email in email_to_name.items():
+    print(f"{name} ({email})")
