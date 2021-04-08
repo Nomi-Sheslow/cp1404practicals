8fe7af34b97b66f71f22476f1e36d882bb8990b0
Nomi-Sheslow <79426752+Nomi-Sheslow@users.noreply.github.com>
2021-04-08 17:20:06 +1000

    Add max_word_length, add if statement to calculate longest word, add formatting to print statement

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -6,6 +6,7 @@ get user text input, put words into dictionary, and print dictionary
 
 word_to_count = {}
 words = []
+max_word_length = 0
 text = input("Text: ").lower()
 words.append(text)
 words = words[0].split(" ")
@@ -15,5 +16,7 @@ for word in words:
         word_to_count[word] = 1
     else:
         word_to_count[word] += 1
+    if len(word) > max_word_length:
+        max_word_length = len(word)
 for word, count in word_to_count.items():
-    print(f"{word} : {count}")
+    print(f"{word:{max_word_length}} : {count}")
