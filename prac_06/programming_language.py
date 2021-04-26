f6f17293ac9dfdc59bbfb8e9f5ea2a764e4c86a2
Nomi-Sheslow <79426752+Nomi-Sheslow@users.noreply.github.com>
2021-04-26 21:23:38 +1000

    Refactor name parameter to name="", refactor typing parameter to typing="", refactor reflection parameter to reflection="", refactor year parameter to year=0, refactor self in __str__ to language; add .format(self), add language=self.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,13 +1,14 @@
 class ProgrammingLanguage:
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name="", typing="", reflection="", year=0):
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __str__(self):
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return "{language.name}, {language.typing} Typing, Reflection={language.reflection}, First appeared in " \
+               "{language.year}".format(language=self)
 
     def is_dynamic(self):
         return self.typing == "Dynamic"
